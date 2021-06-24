9e4164a2a3aeccbe520524e3bf66b4aa3203ffb4
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-06-24 22:42:44 +0900

    htmlをプラグインを追加しました。

--- public/bundle.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";document.querySelector("#root").innerHTML="<p>Hello World!</p>"})();
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,19 @@
 // webpack用の設定ファイル
 const path = require('path');
-// Webpack本体を読み込む
-const webpack = require('webpack');
+// プラグインを読み込む
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    watch: true,
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
     },
-    // ローダーの設定を追加
-    module: {
-        rules: [
-            {
-                test: /\.txt$/,
-                use: 'raw-loader'
-            },
-            {
-                test: /\.js$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env', 'react']
-                    }
-                },
-                // node_modulesフォルダ以下はloaderの対象外とする。
-                exclude: /node_modules/
-            }
-        ]
-    },
     // プラグインの設定を追加
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            extractComments: true
+        new HtmlWebpackPlugin({
+            title: 'Plugin generate page',
+            template: 'src/root.html'
         })
     ]
 };
